24952e62b32bd356821c2ca5194465f7857f5385
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2023-03-27 15:36:07 +0530

    Update leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,3 +1,7 @@
+//4. Write a function which accepts a number as input and then tells if the number is a leap year or not.
+//In case the number is not leap year then it finds the closest leap year to given number. Example, if
+//input is 2015 then it should print, "2015 is not a leap year. After 2015 2016 is next leap year."
+
 const prompt=require("prompt-sync")();
 function isLeapYear(year){
     if(year%4==0 && year%100!=0 || year%400==0){
