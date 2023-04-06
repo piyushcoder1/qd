14fa281c6a9287f48f322a3f7b9df1eabb1d3114
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2023-04-06 15:52:29 +0530

    Create update.js

---
+++ Assignment_Day10-11/update.js
@@ -0,0 +1,14 @@
+const updateNew = (req, res) => {
+  const id = parseInt(req.params.id)
+  const { date ,name} = req.body
+
+  pool.query(
+    'UPDATE students SET date = $6, WHERE id=$1 name = $2',
+    [date, name,id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).send(`Student Data Updated: ${id}`)
+    }
+  )
