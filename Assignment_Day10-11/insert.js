191b35e7234b3a81fcb81a96ada00fc762cde63a
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2023-04-06 15:52:01 +0530

    Create insert.js

---
+++ Assignment_Day10-11/insert.js
@@ -0,0 +1,10 @@
+const insertNew = (req, res) => {
+  const { name, age, gender } = req.body
+
+  pool.query('INSERT INTO students (name, age, gender) VALUES ($1, $2, $3)', [name, age, gender], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`New Student Added: ${results.insertId}`)
+  })
+}
