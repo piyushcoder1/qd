16b396436689b15d2136c92802b34360b511162c
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2023-04-06 15:44:15 +0530

    Create index.js

---
+++ Assignment_Day10-11/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./query')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.displayNew)
+app.post('/users', db.insertNew)
+app.put('/users/:id', db.updateNew)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
